5fa249e320ab69c40e436b14edef25198406e8cc
matti404 <maciej.andraka.ma@gmail.com>
2021-04-25 20:33:23 +0200

    adding-css-loader-to-page

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const btn = document.querySelector('.generate');
 const cats = document.querySelector('.removal-container')
-const cont = document.querySelector('.cats-container');
-
-
+/*const cont = document.querySelector('.cats-container');*/
+const loader = document.querySelector('.loader'); 
+let catsImageDiv = document.querySelector('.cats-images') 
 
 
 
 // Function remove container
 
-const removeContainer = (e) => {
-  e.preventDefault();
+const removeContainer = () => {
+  //e.preventDefault();
   cats.style.display = 'none';
 }
 
@@ -17,31 +17,43 @@ const removeContainer = (e) => {
 // get Cat images
 
 const catsImages = async () => {
-
+   
+    
     try {
-    let catsImageDiv = document.querySelector('.cats-images')  
-    catsImageDiv.innerHTML = '';
-    
-    const res = await fetch('https://api.thecatapi.com/v1/images/search')
+      catsImageDiv.innerHTML = '';
+      showLoader();
+      setTimeout(async () => {
+      const res = await fetch('https://api.thecatapi.com/v1/images/search')
+      const data = await res.json();
+      console.log(data);
+      let catsImage = data[0].url
+      let catsImageEl = document.createElement('img')
+      catsImageEl.setAttribute('src', `${catsImage}`)
+      catsImageEl.classList.toggle('size');
+      catsImageDiv.appendChild(catsImageEl);
 
-    const data = await res.json();
-    console.log(data);
-    let catsImage = data[0].url
-    
-    let catsImageEl = document.createElement('img')
-    catsImageEl.setAttribute('src', `${catsImage}`)
-    catsImageEl.classList.add('size');
+    }, 2000)
+  
+  } catch (e) {
+      console.log('Don`t Worry, Simple Error :p');
+  }
 
-    catsImageDiv.appendChild(catsImageEl);
-
-} catch (e) {
-    console.log('Don`t Worry, Simple Error :p');
 }
-}
 
+
+// show loader
+function showLoader() {
+  loader.style.display = 'block'
+  setTimeout(() => { 
+    loader.style.display = 'none'
+  }, 1000)
+}
 
 // Event Listeners
 
-btn.addEventListener('click', removeContainer);
+btn.addEventListener('click', () => {
+  removeContainer();
+  catsImages();
+});
 
-btn.addEventListener('click', catsImages);
+// btn.addEventListener('click', catsImages);
